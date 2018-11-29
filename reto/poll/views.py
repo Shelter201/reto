0f52f436972ad4d21e7e6e3adec0243d9fbbdca5
León López <leon.lopez@sm4rt.com>
2018-11-28 21:07:26 -0600

    Configuración inicial

--- reto/poll/views.py
+++ reto/poll/views.py
@@ -1,3 +1,15 @@
+# coding=utf-8
 from django.shortcuts import render
+from django.urls import reverse
+from rest_framework import status
+from rest_framework import views, response
 
 # Create your views here.
+
+# =============================================================================
+class IndexView(views.APIView):
+    # -------------------------------------------------------------------------
+    def get(self, request):
+        return response.Response({'resources': [
+        ]})
+
--- reto/reto/settings.py
+++ reto/reto/settings.py
@@ -31,22 +31,20 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'poll'
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+
 ]
 
 ROOT_URLCONF = 'reto.urls'
@@ -75,8 +73,12 @@ WSGI_APPLICATION = 'reto.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Poll_DB',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': 5432,
     }
 }
 
